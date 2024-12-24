21faafffa823202df4cbc29f3353082da55421a8
Afonso Lima <afonsolima@gmail.com>
2024-12-24 08:53:59 -0300

    rendering the schedules

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours";
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map(hour => {
         const [scheduleHour] = hour.split(":")
@@ -16,5 +18,13 @@ export function hoursLoad({ date }) {
         return ret
     })
 
-    return opening
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+        hours.append(li)
+    })
 }
