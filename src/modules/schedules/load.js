a531ab369fba9eff493d6e164a0543840a91a23e
Afonso Lima <afonsolima@gmail.com>
2024-12-24 11:56:10 -0300

    using fetch schedule service

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,8 +1,13 @@
+import { ScheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 import { hoursLoad } from "../form/hours-load";
 
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
+
+    const dailySchedules = await ScheduleFetchByDay({ date })
+    console.log(dailySchedules)
+
     hoursLoad({ date })
 }
