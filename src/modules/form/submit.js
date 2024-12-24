a3bd65e267be15d1def4c26a0258041bb078896a
Afonso Lima <afonsolima@gmail.com>
2024-12-24 10:07:19 -0300

    sending selected schedule

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 const initialDate = dayjs(new Date()).format("YYYY-MM-DD")
@@ -10,5 +11,27 @@ selectedDate.min = initialDate
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log("submit")
+    try {
+        const name = clientName.value.trim()
+        if (!name) {
+            return alert("Informe o nome do cliente.")
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if (!hourSelected) {
+            return alert("Selecione um horário.")
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        const id = new Date().getTime()
+
+        console.log({ id, name, when })
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
